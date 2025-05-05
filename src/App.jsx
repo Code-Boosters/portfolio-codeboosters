39beb2279db60a3f871042aa339c201d057bb91e
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-05-04 22:12:43 -0500

    feat(core): Implement atomic design structure in React portfolio and remove index.css.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const saludo = ()=>{
+    return (
+    <>
+        <h1 class="text-3xl font-bold underline bg-red-500 text-center py-2 text-white">
+        Hello world!
+        </h1>
+    </>
+    )
+}
+
+export default saludo
