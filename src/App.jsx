503d73c27a1c21513946bbf117e33ad43b55a259
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-05-06 21:40:44 -0500

    feat: Whatsapp button added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ButtonWhatsApp from "./components/atoms/ButtonWhatsApp/ButtonWhatsApp";
 
 const saludo = ()=>{
     return (
@@ -6,6 +7,7 @@ const saludo = ()=>{
         <h1 class="text-3xl font-bold underline bg-red-500 text-center py-2 text-white">
         Hello world!
         </h1>
+        <ButtonWhatsApp phoneNumber={3154488668} />
     </>
     )
 }
